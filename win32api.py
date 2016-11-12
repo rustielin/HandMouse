0009dcdae6ae06f660f5cc13ef9c55252a9a6c71
Nadir Akhtar <nadirakhtar123@gmail.com>
2016-11-12 04:10:07 -0800

    Added more functionality (untested)

--- win32api.py
+++ win32api.py
@@ -1,6 +1,51 @@
 import win32api, win32con
 
+
+clicked = False
+
 def left_click():
+    if clicked:
+        return
     x, y = win32api.GetCursorPos()
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0) #click is true
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0) #unclicks
+    clicked = True
+
+def left_press():
+    if clicked:
+        return
+    x, y = win32api.GetCursorPos()
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0) #click is true
+    clicked = True
+
+def left_unpress():
+    x, y = win32api.GetCursorPos()
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0) #unclicks
+    clicked = False
+
+def right_click():
+    if clicked:
+        return
+    x, y = win32api.GetCursorPos()
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0) #click is true
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, x, y, 0, 0) #unclicks
+    clicked = True
+
+def right_press():
+    if clicked:
+        return
+    x, y = win32api.GetCursorPos()
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0) #click is true
+    clicked = True
+
+def right_unpress():
+    x, y = win32api.GetCursorPos()
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, x, y, 0, 0) #unclicks
+    clicked = False
+
+
+def set_mouse(x, y):
+    win32api.SetCursorPos((x, y))
+
+def scroll(direction):
+    if direction == "up":
